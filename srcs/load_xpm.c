d3c2e0dfc71f56475641efc09cce974d2cff7f0c
Victor GAUTHEREAU <vgauther@e1r8p21.42.fr>
2019-11-22 14:28:02 +0100

    opti

--- srcs/load_xpm.c
+++ srcs/load_xpm.c
@@ -6,7 +6,7 @@
 /*   By: vgauther <vgauther@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 17:32:07 by vgauther          #+#    #+#             */
-/*   Updated: 2019/11/22 12:36:47 by vgauther         ###   ########.fr       */
+/*   Updated: 2019/11/22 14:26:21 by vgauther         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,6 @@ SDL_Surface		*load_xpm(char *name, t_xpm_init xpm)
 	local_messages(0);
 	s = create_surface_from_data(&data);
 	local_messages(1);
-	free_color_built_in(&data);
 	free(data.file_name);
 	free_pixels(&data);
 	free_colors(&data);
